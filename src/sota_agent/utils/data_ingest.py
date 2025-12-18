992232819e303663e87af639f06b0c9c6c89095f
Hock <31558937+enghock1@users.noreply.github.com>
2025-12-18 07:31:05 -0600

    restructure utils to separate data ingestion and filtering

--- src/sota_agent/utils/data_ingest.py
+++ src/sota_agent/utils/data_ingest.py
@@ -29,24 +29,3 @@ def stream_arxiv_data(file_path: Path) -> Generator[Dict, None, None]:
                 continue
 
 
-def filter_paper(paper: Dict, target_dataset_config: Dict) -> bool:
-    """
-    Paper filtering logic.
-    """
-    # Category Check
-    # ArXiv categories are space-separated strings, e.g., "cs.LG stat.ML"
-    categories = paper.get('categories', '')
-    if "cs.LG" not in categories and "stat.ML" not in categories:
-        return False
-        
-    # Keyword Check
-    abstract = paper.get('abstract', '').lower()
-    keywords = [k.lower() for k in target_dataset_config.get('keywords', [])]
-
-    # TODO: title check
-    # TODO: date check
-    # TODO: must contain doi
-
-
-    # Returns True if ANY keyword is found
-    return all(keyword in abstract for keyword in keywords)
